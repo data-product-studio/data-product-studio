23c8bc2e7b4c6ab0ef10d81c7416f3193b33cf06
sbankesCC <sbankes@clarkstonconsulting.com>
2022-07-20 14:57:09 -0400

    remove extra experimental code

--- pages/02_Business Logic.py
+++ pages/02_Business Logic.py
@@ -5,8 +5,3 @@ st.title("Business Logic")
 
 # radio buttons to choose logic type
 lan_choice = st.radio( "Choose Format:", ('Written Logic', 'Pseudo Code'))
-
-if lan_choice == "Written Logic":
-    st.dataframe(df[["Step", "Written Logic", "TaxonomyLink"]])
-elif lan_choice == "Pseudo Code":
-    st.dataframe(df[["Step", "Pseudo Code", "TaxonomyLink"]])

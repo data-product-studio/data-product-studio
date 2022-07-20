1160655681f51df62999a2089f08f86d014c7b20
sbankesCC <sbankes@clarkstonconsulting.com>
2022-07-20 13:58:27 -0400

    Added radio button to choose logic display type

--- pages/02_Business Logic.py
+++ pages/02_Business Logic.py
@@ -1,3 +1,11 @@
 import streamlit as st
 st.title("Business Logic")
 "### This page is in progress."
+
+# radio buttons to choose logic type
+lan_choice = st.radio( "Choose Format:", ('Written Logic', 'Pseudo Code'))
+
+if lan_choice == "Written Logic":
+    st.dataframe(df[["Step", "Written Logic", "TaxonomyLink"]])
+elif lan_choice == "Pseudo Code":
+    st.dataframe(df[["Step", "Pseudo Code", "TaxonomyLink"]])

9917d72ad755512e5a25ec7579f45a1dc9d19706
sbankesCC <sbankes@clarkstonconsulting.com>
2022-07-21 12:39:13 -0400

    Added input sanitation for user form submit

--- pages/01_Objectives.py
+++ pages/01_Objectives.py
@@ -11,7 +11,7 @@ status = st.selectbox("\n\nObjective:", ["Calculate Employee Tenure"])
 
 # open Existing Objectives file and display
 df = pd.read_csv("data/objectives.csv")
-sldf = st.dataframe(df)
+sldf = st.table(df)
 
 # form to collect new data
 with st.form("objectives_form",clear_on_submit = True ):
@@ -23,12 +23,17 @@ with st.form("objectives_form",clear_on_submit = True ):
     submitted = st.form_submit_button("Add Key Result")
     # on button submit
     if submitted:
-        # caclulate next ID
-        id = 1 if math.isnan(df["ID"].max()) else df["ID"].max()+1
-        new_data = {'Key Results':key_res, "Status":status, "Created":created_date, "ID": id}
-        # append new data to df
-        df = df.append(new_data, ignore_index = True)
+        if not key_res:
+            st.error("Key Result may not be empty.")
+        elif created_date > datetime.today().date():
+            st.error("Created date must not be in the future")
+        else:
+            # caclulate next ID
+            id = 1 if math.isnan(df["ID"].max()) else df["ID"].max()+1
+            new_data = {'Key Results':key_res, "Status":status, "Created":created_date, "ID": id}
+            # append new data to df
+            df = df.append(new_data, ignore_index = True)
 
-        df.to_csv("data/objectives.csv", index=False)
-        # reruns the script to result form and update table
-        st.experimental_rerun()
+            df.to_csv("data/objectives.csv", index=False)
+            # reruns the script to result form and update table
+            st.experimental_rerun()

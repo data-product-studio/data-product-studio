f1371dd4597f91a159ae74f223b6bc364f49cefd
sbankesCC <sbankes@clarkstonconsulting.com>
2022-09-27 09:00:26 -0400

    Updated Data product management
    Updated logic to fix bugs where select bars are not displaying correctly. Added objective select/dropdown.

--- pages/01_1. DATA PRODUCT MANAGMENT.py
+++ pages/01_1. DATA PRODUCT MANAGMENT.py
@@ -19,65 +19,6 @@ except:
     st.error("Cannot Connect to Database. Check Connection and Try Again")
     st.stop()
 
-project = ut.exectute_query_with_results("SELECT * FROM project", conn)
-objective = ut.exectute_query_with_results("SELECT * FROM objective", conn)
-key_result = ut.exectute_query_with_results("SELECT * FROM keyResult", conn)
-
-# set up tables in db if not already existing
-ut.execute_query("data/objective_DDL.sql", conn, f = True)
-
-
-
-############# DISPLAY PAGE #############
-st.title("DATA PRODUCT MANAGEMENT")
-st.write("Use this page to set data product Objectives and Key Results. \
-Start by selecting or creating a new project. Then, define your project Objectives,\
-or the goals of your project. Finish by adding Key Results for each objective,\
-which should encompass the ideal outcome for all users of your product.")
-
-############# PROJECT SECTION #############
-# if there are no project we must first add a project
-if not project:
-    st.write('You currently have no projects. Click "Add a Project" to Begin.')
-else:
-    # display all projects
-    project_names = [x[1] for x in project]
-    current_project = st.selectbox("Select a Project", project_names)
-
-# if st.button('Add a Project'):
-with st.form("project_form"):
-    project_name = st.text_input("Enter New Project Name")
-
-    # Every form must have a submit button.
-    submitted = st.form_submit_button("Submit Project")
-    if submitted:
-        # q = "INSERT INTO project (project_name) \
-        # VALUES ('{project_name}');".format(project_name = project_name)
-        # execute_query(q, conn)
-        # # st.experimental_rerun()
-        # st.write("Project Sucessfully Added!")
-        st.write("Submitted :) ")
-
-
-
-
-# if not objective:
-#     st.write('You currently have no objectives for this project. Click "New Project Objective".')
-# else:
-#     objective_names = [x[1] for x in objective]
-#     current_project = st.selectbox("Select a objective", objective_names)
-#
-# if st.button('Add an Objective'):
-#     with st.form("objective_form"):
-#         project_name = st.text_input("Enter New Objective Name")
-#
-#         # Every form must have a submit button.
-#         submitted = st.form_submit_button("Submit Objective")
-#         if submitted:
-#             q = "INSERT INTO objective (objective_name) \
-#             VALUES ('{project_name}');".format(objective_name = objective_name)
-#             execute_query(q, conn)
-#             st.experimental_rerun()
 # initalize session states
 if 'Global Project' not in st.session_state:
     st.session_state['Global Project'] = False
@@ -132,3 +73,186 @@ def reset_objective_state():
 
 
 
+########################## PAGE HEADER ##########################
+
+st.write("## DATA PRODUCT MANAGEMENT")
+# TO DO FIX
+st.markdown(""" - Define how an objective creates business value
+- Align teams on the purpose and meaning of those objectives.
+- Key results are how your team defines success so that you know it is done.
+""")
+
+st.write("Tip: Ask for team members' perspectives on the performance reviews of systems and their efficiencies, ask about their pain points using data.")
+
+# st.write(st.session_state)
+# message to displacy if we have a Global Project SELECTED
+status_slot = st.empty()
+
+default_ix = True
+########################## OBJECTIVE BODY ##########################
+
+# get the project selected in "getting started"
+current_project = st.session_state['Global Project']
+
+#reset_objective_state() -- problem line
+# if there is currently a project, then we can procced
+
+if current_project:
+    # query the objectives for the choosen project
+    # find the id for the current project
+    q = "select project_id from project where project_name = '{}'".format(current_project)
+    current_project_id = ut.exectute_query_with_results(q, conn)[0][0]
+    q = """SELECT * FROM objective o JOIN project p
+            ON o.project_id = p.project_id
+            WHERE o.project_id = {pid}""".format(pid = current_project_id)
+
+    objectives = ut.exectute_query_with_results(q, conn)
+    objective_names = [x[1] for x in objectives]
+
+    if st.session_state['Objective Delete Cancelled']:
+        st.warning("Objective Delete Cancelled")
+        st.session_state['Objective Delete Cancelled'] = False
+
+    if st.session_state['Objective Delete Confirm']:
+        st.success("Objective Delete Successful")
+        st.session_state['Objective Delete Confirm'] = False
+
+    if not objectives:
+        st.write('You currently have no objectives for this project. Click "Add Objective" to Begin.')
+        st.session_state["Global Objective"] = False
+        ut.update_message_slot(status_slot)
+    else:
+        # display any flags
+        if st.session_state['Objective Submitted']:
+            default_ix = False
+            st.success("Objective Sucessfully Submitted!")
+            st.session_state['Objective Submitted'] = False
+        if st.session_state['Objective Edit Confirm']:
+            default_ix = False
+            st.success("Objective Edit Successful")
+            st.session_state['Objective Edit Confirm'] = False
+
+        # display all projects
+        objectives_names = [x[1] for x in objectives]
+        objectives_ids = [x[0] for x in objectives]
+        # If a project was just added or edited, make that the default selection
+        if default_ix:
+            default_ix = 0
+        else:
+            default_ix = len(objectives_names) -1
+        current_objective = st.selectbox("Objective", objectives_names, index = default_ix)
+        st.session_state['Global Objective'] = current_objective
+        ut.update_message_slot(status_slot)
+    # create column of buttons for project options
+    col1, col2, col3  = st.columns(3)
+
+    # hiding view for now, might want later
+    # with col1:
+    #     view = st.button("View Projects", on_click = set_project_view_button)
+    with col1:
+        add = st.button('Add an Objective', on_click = set_objective_add_button)
+    if objectives:
+        with col2:
+            edit = st.button("Edit Objective", on_click = set_objective_edit_button)
+        with col3:
+            delete = st.button("Delete Objective", on_click = set_objective_delete_button)
+
+    # Add button is clicked
+    if st.session_state['Add an Objective']:
+        with st.form("objective_form"):
+            # initialize entry sanitization
+            valid_entry = True
+            reason = ""
+            # ask user to enter new name
+            new_objective_name = st.text_input("Enter New Objective")
+            if objectives and (new_objective_name.lower() in [x.lower() for x in objective_names]):
+                valid = True
+                reason = "Objective already exists for this project"
+            elif len(new_objective_name) < 2:
+                valid = True
+                reason = "Objective must be at least 2 characters."
+            else:
+                valid = True
+            # Every form must have a submit button.
+            submitted = st.form_submit_button("Submit")
+            if submitted:
+                if not valid:
+                    st.error(reason)
+                else:
+                    q = "INSERT INTO objective (objective_name, project_id) \
+                    VALUES ('{objective_name}', {project_id});".format(objective_name = new_objective_name, project_id = current_project_id)
+                    # st.write(q)
+                    ut.execute_query(q, conn)
+                    # update session states
+                    reset_objective_state()
+                    st.session_state['Objective Submitted'] = True
+                    st.experimental_rerun()
+
+    # Edit button is Clicked
+    elif (st.session_state['Edit Objective']) and objectives:
+        # st.warning('You are editing "{objective_name}"'.format(objective_name = current_objective))
+        with st.form("ObjectiveEditForm"):
+            # initalize input sanitization
+            valid_entry = True
+            reason = ""
+            edit_name = st.text_input("What would you like to rename your objective?",value = current_objective)
+            # Validate edited name
+            if edit_name.lower() in [x.lower() for x in objective_names]:
+                valid = False
+                reason = "Objective name already exists"
+            elif len(edit_name) < 1:
+                valid = False
+                reason = "Objective name must be at least 2 characters."
+            else:
+                valid = True
+            # Every form must have a submit button.
+            submitted = st.form_submit_button("Edit Project")
+            if submitted:
+                if not valid:
+                    st.error(reason)
+                else:
+                    q = "UPDATE objective set objective_name = '{new_name}' where objective_name = '{current_objective}';".format(new_name = edit_name, current_objective = current_objective)
+                    ut.execute_query(q, conn)
+                    reset_objective_state()
+                    st.session_state['Objective Edit Confirm'] = True
+                    st.experimental_rerun()
+    elif (st.session_state["Delete Objective"]) and objectives:
+            # use error message to confirm delte
+            st.error('Do you wish to delete "{current_objective}" ?'.format(current_objective = current_objective))
+            # c
+            space, no, yes, space  = st.columns(4)
+            with no:
+                no_button = st.button("NO")
+            with yes:
+                yes_button = st.button("YES (DELETE PROJECT)" )
+
+            # if the users says no, set a
+            if no_button:
+                reset_objective_state()
+                st.session_state["Objective Delete Cancelled"] = True
+                st.experimental_rerun()
+            elif yes_button:
+                # TODO: FIX TO HANDLE IF MULTIPLE PROJECTS NAMES ARE THE SAME
+                q = "DELETE FROM objective WHERE objective_name = '{current_objective}';".format(current_objective = current_objective)
+                # st.write(q)
+                ut.execute_query(q, conn)
+                reset_objective_state()
+                st.session_state["Objective Delete Confirm"] = True
+                st.experimental_rerun()
+
+st.markdown("""---""")
+# reset_objective_state() ##I put this here randomly because otherwise if you hit add objective and then try to reset, it keeps evaluating to true
+########################## KEY RESULT BODY ##########################
+
+## To DoL
+if current_project and st.session_state['Global Objective']:
+    # st.session_state['Global Objective']
+    q = "select objective_id from objective where objective_name = '{}'".format(st.session_state['Global Objective'])
+    current_objective_id = ut.exectute_query_with_results(q, conn)[0][0]
+    q = """SELECT * FROM keyResult k JOIN objective o
+            ON k.objective_id = o.objective_id
+            WHERE k.objective_id = {oid}""".format(oid = current_objective_id)
+
+    objectives = ut.exectute_query_with_results(q, conn)
+
+    st.write(objectives)

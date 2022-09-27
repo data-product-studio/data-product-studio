85ab042b20e1e329bdeea03a6911c234049c4978
sbankesCC <sbankes@clarkstonconsulting.com>
2022-09-27 08:54:35 -0400

    Updated DDL Script for Cascade Delete

--- data/objective_DDL.sql
+++ data/objective_DDL.sql
@@ -1,17 +1,23 @@
 CREATE TABLE IF NOT EXISTS  project(
    project_id SERIAL PRIMARY KEY,
-   project_name text NOT NULL
+   project_name text NOT NULL,
+  CONSTRAINT project_name_unique UNIQUE (project_name)
 );
 
 CREATE TABLE IF NOT EXISTS objective(
    objective_id SERIAL PRIMARY KEY,
    objective_name text NOT NULL,
-   project_id serial REFERENCES project
+   project_id serial
 );
 
 CREATE TABLE IF NOT EXISTS keyResult(
    keyResult_id SERIAL PRIMARY KEY,
    keyResult_definition text NOT NULL,
    keyResult_status text NOT NULL,
-   objective_id serial REFERENCES objective
+   objective_id serial
 );
+
+ALTER TABLE objective DROP CONSTRAINT IF EXISTS "objective_project_fk";
+ALTER TABLE objective ADD CONSTRAINT "objective_project_fk" FOREIGN KEY(project_id) REFERENCES project ON DELETE CASCADE;
+ALTER TABLE keyResult DROP CONSTRAINT IF EXISTS "keyResult_objective_fk";
+ALTER TABLE keyResult ADD CONSTRAINT "keyResult_objective_fk" FOREIGN KEY(objective_id) REFERENCES objective ON DELETE CASCADE;
